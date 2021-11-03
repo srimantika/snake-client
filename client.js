5436afa2dede4a5d3c96adb0917d88c38103eaf7
srimantika <srimantika@gmail.com>
2021-11-03 20:09:21 +0000

    Add constants file

--- client.js
+++ client.js
@@ -1,8 +1,9 @@
+const { IP, PORT } = require('./constants');
 const net = require('net');
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541',
+    host: IP,
+    port: PORT,
   });
 
   // interpret incoming data as text
@@ -11,6 +12,10 @@ const connect = function () {
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
     conn.write("Name: Sri");    
+  });
+
+  conn.on('data', data => {
+    console.log(data);
   });
 
   return conn;
