e4bc5782526d02c1d064e3d2b9ec415c87b6d142
srimantika <srimantika@gmail.com>
2021-11-03 17:47:11 +0000

    studied move commands

--- client.js
+++ client.js
@@ -10,7 +10,7 @@ const connect = function () {
 
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
-    conn.write("Name: Sri");
+    conn.write("Name: Sri");    
   });
 
   return conn;
