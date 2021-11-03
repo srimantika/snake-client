14489cf2b5f6890fe33aef77fd5854870b47fe99
srimantika <srimantika@gmail.com>
2021-11-03 18:19:54 +0000

    stdin setup

--- play.js
+++ play.js
@@ -1,5 +1,28 @@
 const { connect } = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', key => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+
+  if (key === '\u0003') {
+    stdout.write("Exited snake game.\n");
+    process.exit();
+  }
+};
+
+
+
+
+
 
 console.log("Connecting ...");
 connect();
