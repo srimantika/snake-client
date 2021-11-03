bd3139d0517bf15491ad770e8f06ce24cb338f18
srimantika <srimantika@gmail.com>
2021-11-03 17:31:08 +0000

    Adding snake initials and connected message

--- client.js
+++ client.js
@@ -8,6 +8,11 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: Sri");
+  });
+
   return conn;
 };
 
